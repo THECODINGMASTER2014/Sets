65e2965bedd7307f9e3ca038929b363b8ad2c140
THECODINGMASTER2014 <talwarmoksh@gmail.com>
2026-03-15 11:28:23 +0530

    a

---
+++ Index2.py
@@ -0,0 +1,9 @@
+setx = {'green', 'yellow', 'blue', 'red'}
+sety = {'green', 'yellow', 'violet', 'indigo'}
+print("ORignal Set Elements :")
+print(setx)
+print(sety)
+print("\n Intersection of two said Sets")
+setz = setx.intersection(sety)
+print(setz)
+
